c62a48e8d86f8fc28c043f50524ff66cb986b6df
uyld@itsol.vn <uyld@itsol.vn>
2023-03-28 21:07:47 +0700

    1

---
+++ src/Maii.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Maii {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        if (n == 1) System.out.println(4);
+        else if (n == 2) System.out.println(7);
+        else if (n == 3) System.out.println(5);
+        else System.out.println(triBoNaCci(n));
+    }
+
+    public static long triBoNaCci(int n) {
+        long[] arr = new long[n + 1];
+        n = n - 1;
+        arr[0] = 4;
+        arr[1] = 7;
+        arr[2] = 5;
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+        }
+        return arr[n];
+    }
+}
+
